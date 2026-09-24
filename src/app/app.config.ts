2c74b6ed46a3f6c4a5a3a747ea68ab9b04853862
Samuel Astudillo <159580891+Swenndy@users.noreply.github.com>
2026-09-24 15:42:21 -0300

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-=======
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
->>>>>>> 7e882a8e7e115f956b1115dad784466cb8e6c741
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-<<<<<<< HEAD
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
-  ],
-=======
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch())
   ]
->>>>>>> 7e882a8e7e115f956b1115dad784466cb8e6c741
 };
