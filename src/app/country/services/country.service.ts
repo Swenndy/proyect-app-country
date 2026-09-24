70cd2b3c6e7d0865773674580569f24969dcd1b7
Samuel Astudillo <159580891+Swenndy@users.noreply.github.com>
2026-09-24 15:49:00 -0300

    Update country.service.ts

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,23 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-<<<<<<< HEAD
 
 import { RESTCountry } from '../interfaces/rest-countries.interface';
 import { map, Observable, catchError, throwError, delay, of, tap } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 import { Region } from '../interfaces/region.type';
-=======
-import { RESTCountry } from '../interfaces/rest-countries.interfaces';
-import { catchError, map, Observable, throwError } from 'rxjs';
-import { Country } from '../interfaces/country.interface';
-import { CountryMapper } from '../mappers/country.mapper';
->>>>>>> 7e882a8e7e115f956b1115dad784466cb8e6c741
 
 const API_URL = 'https://restcountries.com/v3.1';
 
 @Injectable({
-<<<<<<< HEAD
   providedIn: 'root',
 })
 export class CountryService {
@@ -25,18 +17,10 @@ export class CountryService {
   private queryCacheCapital = new Map<string, Country[]>();
   private queryCacheCountry = new Map<string, Country[]>();
   private regionCache = new Map<string, Country[]>();
-=======
-  providedIn: 'root'
-})
-export class CountryService {
-
-  private http = inject(HttpClient);
->>>>>>> 7e882a8e7e115f956b1115dad784466cb8e6c741
 
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
 
-<<<<<<< HEAD
     if (this.queryCacheCapital.has(query)) return of(this.queryCacheCapital.get(query) ?? []);
 
     console.log(`LLegando al servidor por ${query}`);
@@ -101,34 +85,5 @@ export class CountryService {
       map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
       tap((countries) => this.regionCache.set(region, countries)),
     )
-
   }
-=======
-    return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`).pipe(
-      map((resp) => CountryMapper.formap(resp)),
-      catchError(error => {
-        console.log('Error fetching', error);
-
-        return throwError(() => new Error('No se pudo obtener paises con ese query'))
-      })
-
-    );
-  }
-
-  searchbyCountry(query: string) {
-
-    query = query.toLowerCase();
-    const url = `${API_URL}/name/${query}`;
-
-    return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`).pipe(
-      map((resp) => CountryMapper.formap(resp)),
-      catchError(error => {
-        console.log('Error fetching', error);
-
-        return throwError(() => new Error('No se pudo obtener paises con ese query'))
-      })
-
-    );
-  }
->>>>>>> 7e882a8e7e115f956b1115dad784466cb8e6c741
 }
