37ebe32831fd52ae5334699d6ac2bbc46823e175
Samuel Astudillo <159580891+Swenndy@users.noreply.github.com>
2026-09-24 15:52:00 -0300

    Update country.routes.ts

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,23 +1,13 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { CountryLayoutComponent } from './layouts/CountryLayout/CountryLayout.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 import { CountryPageComponent } from './pages/country-page/country-page.component';
-=======
-import { ByCapitalPage } from './pages/by-capital-page/by-capital-page';
-import { CountryLayout } from './layouts/CountryLayout/CountryLayout';
-import { ByCountryPage } from './pages/by-country-page/by-country-page';
-import { ByRegionPage } from './pages/by-region-page/by-region-page';
-import { CountryPage } from './pages/country-page/country-page';
-
->>>>>>> 7e882a8e7e115f956b1115dad784466cb8e6c741
 
 export const countryRoutes: Routes = [
   {
     path: '',
-<<<<<<< HEAD
     component: CountryLayoutComponent,
     children: [
       {
@@ -32,45 +22,16 @@ export const countryRoutes: Routes = [
         path: 'by-region',
         component: ByRegionPageComponent,
       },
-
       {
         path: 'by/:code',
         component: CountryPageComponent,
       },
-
       {
         path: '**',
         redirectTo: 'by-capital',
       },
     ],
   },
-=======
-    component: CountryLayout,
-    children: [
-      {
-        path: 'by-capital',
-        component: ByCapitalPage,
-      },
-      {
-        path: 'by-country',
-        component: ByCountryPage,
-      },
-      {
-        path: 'by-region',
-        component: ByRegionPage,
-      },
-      {
-        path: 'by/:code',
-        component: CountryPage,
-      },
-      {
-        path: '**',
-        redirectTo: 'by-capital',
-      }
-    ]
-  },
-
->>>>>>> 7e882a8e7e115f956b1115dad784466cb8e6c741
 ];
 
 export default countryRoutes;
